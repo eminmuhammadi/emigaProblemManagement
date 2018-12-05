d9ea5c46fd0fb3664d8e6a7691503494c0c9bd2a
Emin Mühəmmədi <muemin17631@sabah.edu.az>
2018-12-05 10:36:45 +0400

    EmiGA Problem Management

--- template/head.php
+++ template/head.php
@@ -1,6 +1,6 @@
 <?php header('Content-Type: text/html; charset=utf-8'); ?>
 <!DOCTYPE html>
-<html lang="en">
+<html lang="en" class="no-js">
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
@@ -12,6 +12,5 @@
   <!-- All -->
   <link rel="stylesheet" href="/<?php echo $_COOKIE['emigaUniqID'] ;?>-app.css">
   <link rel="stylesheet" type="text/css" href="/static/pack/vendors/iconfonts/simple-line-icon/css/simple-line-icons.css">
-
 </head>
 <body>
